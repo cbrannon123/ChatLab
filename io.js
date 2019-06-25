a20bf617355bddd54c12cc83e8a0e2d3139fccdf
Casey Brannon <cbrannon412@gmail.com>
2019-06-24 19:06:20 -0700

    firstcommit'

---
+++ io.js
@@ -0,0 +1,20 @@
+var io = require('socket.io')();
+io.on('connection', function (socket) {
+    socket.on('add-message', function(data) {
+        io.emit('add-message', data);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = io;
