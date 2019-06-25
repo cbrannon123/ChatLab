6e055b0c328ad3168df3e2a5e4704fcf75ea3872
Casey Brannon <cbrannon412@gmail.com>
2019-06-24 19:48:54 -0700

    textbox

--- io.js
+++ io.js
@@ -1,4 +1,7 @@
 var io = require('socket.io')();
+var players = {};
+
+
 io.on('connection', function (socket) {
     socket.on('add-message', function(data) {
         io.emit('add-message', data);
